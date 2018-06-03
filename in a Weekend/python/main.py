04d19cf82f1f5db1bb612b5c8757a1ead8ab889d
Qinsi ZHU <qszhu2006@gmail.com>
2018-06-03 16:35:29 +0800

    ch4

--- in a Weekend/python/main.py
+++ in a Weekend/python/main.py
@@ -1,7 +1,17 @@
 from vec3 import Vec3
 from ray import Ray
 
+def hit_sphere(center, radius, r):
+    oc = r.origin - center
+    a = Vec3.Dot(r.direction, r.direction)
+    b = 2.0 * Vec3.Dot(oc, r.direction)
+    c = Vec3.Dot(oc, oc) - radius*radius
+    discriminant = b*b - 4*a*c
+    return discriminant > 0
+
 def color(r):
+    if hit_sphere(Vec3(0, 0, -1), 0.5, r):
+        return Vec3(1, 0, 0)
     unit_direction = Vec3.Unit(r.direction)
     t = 0.5*(unit_direction.y + 1.0)
     return (1.0-t)*Vec3(1.0, 1.0, 1.0) + t*Vec3(0.5, 0.7, 1.0)
