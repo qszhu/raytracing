730a55b0853e13d28f2e997722e1f79607405325
Qinsi ZHU <qszhu2006@gmail.com>
2018-06-03 16:58:53 +0800

    ch7

--- in a Weekend/python/main.py
+++ in a Weekend/python/main.py
@@ -1,14 +1,22 @@
+import math
 import sys
 from random import random
 
 from hitable import HitRecord, HitableList, Sphere
 from vec3 import Vec3
+from ray import Ray
 from camera import Camera
+
+def random_in_unit_sphere():
+    while True:
+        p = 2.0*Vec3(random(),random(),random()) - Vec3(1,1,1)
+        if p.dot(p) < 1: return p
 
 def color(r, world):
     rec = HitRecord()
-    if world.hit(r, 0.0, sys.float_info.max, rec):
-        return 0.5*Vec3(rec.normal.x+1, rec.normal.y+1, rec.normal.z+1)
+    if world.hit(r, 0.001, sys.float_info.max, rec):
+        target = rec.p + rec.normal + random_in_unit_sphere()
+        return 0.5*color(Ray(rec.p, target-rec.p), world)
 
     unit_direction = Vec3.Unit(r.direction)
     t = 0.5*(unit_direction.y + 1.0)
@@ -32,6 +40,7 @@ def main():
                 r = cam.getRay(u, v)
                 col += color(r, world)
             col /= float(ns)
+            col = Vec3(math.sqrt(col[0]), math.sqrt(col[1]), math.sqrt(col[2]))
             ir, ig, ib = int(255.99*col[0]), int(255.99*col[1]), int(255.99*col[2])
             print ir, ig, ib
 
